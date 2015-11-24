b661ebf4de84eb5f5f5dfc8303485ca8f7d023cf
Michael Tse <michael.tse3@gmail.com>
2015-11-24 11:50:00 -0800

    added client side

--- michael_tse/gulpfile.js
+++ michael_tse/gulpfile.js
@@ -3,6 +3,7 @@ var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 var appFiles = ['server.js', 'lib/**/*.js'];
 var testFiles = ['./test/**/*.js'];
+var webpack = require('webpack-stream');
 
 gulp.task('jshint:test', function(){
   return gulp.src(testFiles)
@@ -29,5 +30,23 @@ gulp.task('mocha', ['jshint'], function(){
   .pipe(mocha({reporter:'spec'}));
 });
 
+gulp.task('static:dev', function() {
+  gulp.src('app/**/*.html')
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('webpack:dev', function() {
+  return gulp.src('app/js/entry.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('build/'));
+});
+
+
 gulp.task('jshint', ['jshint:test', 'jshint:app']);
 gulp.task('default', ['jshint', 'mocha']);
+gulp.task('build:dev', ['webpack:dev', 'static:dev']);
+gulp.task('default', ['build:dev']);
