a7b316bbd1ef7cca12e236c2ff033ee3d93e1422
Michael Tse <michael.tse3@gmail.com>
2015-11-24 12:40:22 -0800

    adjusted some of the text

--- michael_tse/app/js/entry.js
+++ michael_tse/app/js/entry.js
@@ -2,7 +2,7 @@ require('angular/angular');//don't save into variable! it won't work
 var angular = window.angular;
 
 //make angular app
-var playerApp = angular.module('bearstream', []);
+var playerApp = angular.module('mlbplayer', []);
 //want $scope as 1st parameter of function. how to do dependency injection
 playerApp.controller('GreetingController', ['$scope', function($scope) {
   $scope.greeting = 'Go Mariners';
