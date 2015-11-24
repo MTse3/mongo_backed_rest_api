d58039c48e0208e56436358e733a63a7865ba15d
Michael Tse <michael.tse3@gmail.com>
2015-11-24 11:03:09 -0800

    added some auth saved bcrypt and eat needs testing and others...

--- michael_tse/server.js
+++ michael_tse/server.js
@@ -2,10 +2,12 @@ var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 var playerRouter = require(__dirname + '/routes/player_routes');
+var authRouter = require(__dirname + '/routes/auth_routes');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/mlb_player')
 
 app.use('/api', playerRouter);
+app.use('/api', authRouter);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server running');
