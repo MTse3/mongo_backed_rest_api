b074d8d1cd31a8447c7f51f464c97b50595156f4
Michael Tse <michael.tse3@gmail.com>
2015-11-28 19:39:35 -0800

    changed default greeting name

--- michael_tse/app/js/entry.js
+++ michael_tse/app/js/entry.js
@@ -1,11 +1,9 @@
-require('angular/angular');//don't save into variable! it won't work
+require('angular/angular');
 var angular = window.angular;
 
-//make angular app
 var playerApp = angular.module('mlbplayer', []);
-//want $scope as 1st parameter of function. how to do dependency injection
 playerApp.controller('GreetingController', ['$scope', function($scope) {
-  $scope.greeting = 'Go Mariners';
+  $scope.greeting = 'Go Mariners!';
 
   $scope.alertGreeting = function() {
     alert($scope.greeting);
